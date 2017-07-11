7287c80974a10e82079c399388deadb0b13c9d58
Hannu-Pekka Heinäjärvi <hannu-pekka.heinajarvi@futurice.com>
2017-07-11 19:12:18 +0300

    Add basic styling.

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ import CSSModules from 'react-css-modules';
 const Header: Component<{}> = (): React$Element<any> =>
   <header styleName="header">
     <h1 styleName="title">Checkem</h1>
+    <button styleName="button">Create new list</button>
   </header>;
 
 export default pure(CSSModules(Header, styles));
